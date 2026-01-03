370754ca887d6bf9435091b6d24fc8d4c0dbedbc
syemie069 <syemie069@gmail.com>
2026-01-03 19:45:04 +0530

    1th commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Todo from "./Todo";
+import "./../components/styles.css";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTodo = () => {
+    if (inputValue.trim() === "") return;
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
 
-export default App
+      <input
+        type="text"
+        value={inputValue}
+        placeholder="Enter todo"
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <button onClick={addTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
